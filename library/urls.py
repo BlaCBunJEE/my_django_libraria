432b8db66d4f79ee43783e346fedb6bcd6a0e9c5
BlaCBunJEE <96791045+BlaCBunJEE@users.noreply.github.com>
2022-09-26 14:31:17 +0100

    the beginning

--- library/urls.py
+++ library/urls.py
@@ -6,6 +6,11 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('media/', views.media, name='media'),
-    path('blog/', views.blog(), name='blog'),
+    path('blog/', views.blog, name='blog'),
+    path('services/', views.services, name='services'),
+    path('contact/', views.contact, name='contact'),
+    path('signin/', views.signin, name='signin'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
 ]
 
--- library/views.py
+++ library/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, redirect
 
 
 # Create your views here.
+def signin(request):
+    context = {}
+    return render(request, 'library/signin.html', context)
+
+
 def home(request):
     context = {}
     return render(request, 'library/index.html', context)
@@ -15,3 +20,26 @@ def media(request):
 def blog(request):
     context = {}
     return render(request, 'library/blog.html', context)
+
+
+def services(request):
+    context = {}
+    return render(request, 'library/services.html', context)
+
+
+def contact(request):
+    context = {}
+    return render(request, 'library/contact.html', context)
+
+
+def cart(request):
+    context = {}
+    return render(request, 'library/cart.html', context)
+
+
+def checkout(request):
+    context = {}
+    return render(request, 'library/checkout.html', context)
+
+
+
